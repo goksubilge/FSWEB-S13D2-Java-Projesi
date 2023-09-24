840ac3706714da147d4cfbaa1b278fa257e19438
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-09-24 23:54:20 +0300

    complete Perfect numbers

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-            System.out.println(isPalindrome(-1221));
+        System.out.println("Palindrom Sayi");
+        System.out.println(isPalindrome(-1221));
         System.out.println(isPalindrome(707));
         System.out.println(isPalindrome(11212));
+        System.out.println("Mukemmel Sayilar");
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+        System.out.println(isPerfectNumber(1));
+        System.out.println(isPerfectNumber(496));
+        System.out.println(isPerfectNumber(8128));
+        System.out.println(isPerfectNumber(33550336));
     }
-
     public static boolean isPalindrome(int num) {
         int reversedNum = 0, remainder;
         int originalNum = num;
@@ -19,4 +27,14 @@ public class Main {
         return originalNum == reversedNum ;
     }
 
+    public static boolean isPerfectNumber(int numb) {
+        if ( numb <0) return false;
+        int sum = 0;
+        for (int i=1; i<numb; i++) {
+            if(numb%i == 0) {
+                sum += i;
+            }
+        }
+        return sum == numb;
+    }
 }
