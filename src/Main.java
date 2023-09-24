fc23667eda494138e407ed9c6f07d298a338b8f4
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-09-24 23:24:44 +0300

    complete Palindrom number

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+            System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
     }
+
+    public static boolean isPalindrome(int num) {
+        int reversedNum = 0, remainder;
+        int originalNum = num;
+
+        while (num != 0) {
+            remainder = num % 10;
+            reversedNum = reversedNum * 10 + remainder;
+            num /= 10;
+        }
+        return originalNum == reversedNum ;
+    }
+
 }
